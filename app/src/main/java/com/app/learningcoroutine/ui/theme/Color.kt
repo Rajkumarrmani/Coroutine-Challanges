b52030a37ccd81b434dbe9d4688235207217b830
RaJManii <rajkumar.m@vivyacorp.com>
2025-08-22 12:01:39 +0530

    Initial commit

---
+++ app/src/main/java/com/app/learningcoroutine/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.app.learningcoroutine.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFF37B98B)
+val textPrimary = Color(0xFF2E3642)
+val textSecondary = Color(0xFF66707F)
+val textDisabled = Color(0xFFB4BDCA)
+val surface = Color(0xFFF4F6F6)
+val surfaceHigher = Color(0xFFFFFFFF)
+val outline25 = Color(0x40FFFFFF)
+val outline0 = Color(0x00FFFFFF)
